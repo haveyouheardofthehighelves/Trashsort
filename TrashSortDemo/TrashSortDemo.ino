60fab1bf0033568d3605817a0d45e197860bddd7
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2024-01-20 21:28:56 -0800

    demo finished added working lcd display

--- TrashSortDemo/TrashSortDemo.ino
+++ TrashSortDemo/TrashSortDemo.ino
@@ -1,11 +1,16 @@
 #include <Servo.h>
+#include <LiquidCrystal.h> 
 Servo myservo;
 int servopin = 9; 
 int ir = A0; 
 int cw = 8;
 int ccw = 10;
 int last_angle = 0;
+int Contrast=75;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
 void setup() {
+  analogWrite(6,Contrast);
+  lcd.begin(16, 2);
   pinMode(servopin, OUTPUT); 
   pinMode(ir, INPUT); 
   pinMode(cw, OUTPUT); 
@@ -14,10 +19,14 @@ void setup() {
   myservo.write(last_angle); 
   Serial.begin(9600); 
   // put your setup code here, to run once:
-
 }
 
 void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("HELLO WORLD!");
+   
+  lcd.setCursor(0, 1);
+  lcd.print("fuck off :]");
   int value = analogRead(ir); 
   Serial.println(value); 
   if (value >= 250){ 
@@ -29,10 +38,9 @@ void loop() {
       last_angle = 0; 
     }
     delay(1500); 
-    digitalWrite(3, HIGH); 
+    digitalWrite(cw, HIGH); 
     delay(500);
-    digitalWrite(3, LOW); 
+    digitalWrite(cw, LOW); 
   }
-  // put your main code here, to run repeatedly:
-
 }
+  // put your main code here, to run repeatedly:
