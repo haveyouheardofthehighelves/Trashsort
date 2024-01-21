5ec80181d79036fb78c68c4a903be25b736a1d25
issy16 <ichen064@ucr.edu>
2024-01-21 00:48:31 -0800

    add unknown cat

--- python/trashsort.py
+++ python/trashsort.py
@@ -4,29 +4,57 @@ from cvzone.ClassificationModule import Classifier
 import serial
 # 0 as trash bin position, 1 as recycle bin position
 current_pos = 0
+current_dir = 'forward'
+
+def writetoarduino(writeall):
+    arr = bytes(writeall, 'utf-8')
+    ser.write(arr)
 
 
 def initial_position():
     global current_pos
     current_pos = 0
+    global current_dir
+    current_dir = 'forward'
 
 
 def set_servo_position(angle):
     ser.write(angle)
 
 
+def set_motor_direction(dir):
+    ser.write(dir)
+
+
 def move_to_trash():
     global current_pos
+    global current_dir
+    if current_dir == 'backward':
+        set_motor_direction(1) # 1 = forward
     if current_pos == 1:
         set_servo_position(0)
     current_pos = 0
+    current_dir = 'forward'
 
 
 def move_to_recycle():
     global current_pos
+    global current_dir
+    if current_dir == 'backward':
+        set_motor_direction(1) # 1 = forward
     if current_pos ==0:
         set_servo_position(1)
     current_pos = 1
+    current_dir = 'forward'
+
+
+def move_back():
+    global current_dir
+    if current_dir == 'forward':
+        set_motor_direction('backward')
+
+
+
 
 
 ser = serial.Serial("COM3", 9600, timeout=1)
@@ -40,6 +68,8 @@ while True:
         move_to_trash()
     if index in(3, 4):
         move_to_recycle()
+    if index in(5):
+        move_back()
 
     cv2.imshow("Image",img)
     cv2.waitKey(1)
