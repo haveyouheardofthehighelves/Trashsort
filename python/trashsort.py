58d9acfe637d42d3974b7126b3722db4fcbb6f40
haveyouheardofthehighelves <32472487+haveyouheardofthehighelves@users.noreply.github.com>
2024-01-21 02:38:19 -0800

    updated logic

--- python/trashsort.py
+++ python/trashsort.py
@@ -2,9 +2,11 @@ import cv2
 import tensorflow
 from cvzone.ClassificationModule import Classifier
 import serial
+
 # 0 as trash bin position, 1 as recycle bin position
 current_pos = 0
 current_dir = 'forward'
+
 
 def writetoarduino(writeall):
     arr = bytes(writeall, 'utf-8')
@@ -30,9 +32,9 @@ def move_to_trash():
     global current_pos
     global current_dir
     if current_dir == 'backward':
-        set_motor_direction(1) # 1 = forward
-    if current_pos == 1:
-        set_servo_position(0)
+        set_motor_direction('%')  # 1 = forward
+    if current_pos == 180:
+        set_servo_position(')')
     current_pos = 0
     current_dir = 'forward'
 
@@ -41,10 +43,10 @@ def move_to_recycle():
     global current_pos
     global current_dir
     if current_dir == 'backward':
-        set_motor_direction(1) # 1 = forward
-    if current_pos ==0:
-        set_servo_position(1)
-    current_pos = 1
+        set_motor_direction('$')  # 1 = forward
+    if current_pos == 0:
+        set_servo_position('(')
+    current_pos = 180
     current_dir = 'forward'
 
 
@@ -55,23 +57,19 @@ def move_back():
     current_dir = 'backward'
 
 
-
-
 ser = serial.Serial("COM3", 9600, timeout=1)
 cap = cv2.VideoCapture(0)
 classifier = Classifier('converted_keras/keras_model.h5', 'converted_keras/labels.txt')
 initial_position()
+detected = False
 while True:
     _, img = cap.read()
     prediction, index = classifier.getPrediction(img)
-    if index in (0, 1, 2):
-        move_to_trash()
-    if index in(3, 4):
-        move_to_recycle()
-    if index in(5):
-        move_back()
+    if detected:
+        if index in (0, 1, 2):
+            move_to_trash()
+        if index in (3, 4):
+            move_to_recycle()
 
-    cv2.imshow("Image",img)
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
-
-
